3763259340789e0b98b14a371d0d55d8edd7fc58
sajiki <m.sajiki@secure-cycle.co.jp>
2023-02-05 22:43:59 +0900

    fix bug but HACK...

--- src/logics/index.ts
+++ src/logics/index.ts
@@ -1,9 +1,12 @@
-export const convertBulletPointsToHyphen = (text: string) => text.replaceAll(/[*|+] /g, '- ');
-
 export const convertBulletPointsWithIndent = (text: string) => {
+  // HACK
   const headText = text.match(/^[\s\S]*?[-|*|+][\s\S]{1}/);
-  const isNumberPoints = text.match(/^[\s\S]*?\d\.[\s\S]{1}/);
-  if (!headText || isNumberPoints) {
+  // const isNumberPoints = text.match(/^[\s\S]*?\d+?\.{1}[\s\S]{1}/);
+  const isNumberPoints2 = text.match(/^\d+?\.{1}[\s\S]{1}/);
+  const estimate = text.match(/\d[h|m]$/);
+  const isNumberPoints = text.replace((estimate || [])[0] || '', '').match(/^[\s\S]*?\d+?\.{1}[\s\S]{1}/);
+
+  if (!headText || isNumberPoints2 || isNumberPoints) {
     return text;
   }
   const convertTabSpace = headText[0].replace(/\t-/g, '-').replaceAll(/\t/g, '-');
@@ -14,14 +17,16 @@ export const convertBulletPointsWithIndent = (text: string) => {
 };
 
 export const convertHeadline = (multiLineText: string) => multiLineText.replaceAll('#', '*');
+
 export const convertNumberPoints = (text: string) => {
   const headText = text.match(/^[\s\S]*?\d\.[\s\S]{1}/);
+
   if (!headText) {
     return text;
   }
   const convertTabSpace = headText[0].replace(/\t-/g, '+').replaceAll(/\t/g, '+');
   const convertHalfWidthSpace = convertTabSpace.replace(/^\s{4}/, '+').replaceAll('  ', '+');
-  const convertHeadNumber = convertHalfWidthSpace.replaceAll(/\d\./g, '+');
+  const convertHeadNumber = convertHalfWidthSpace.replaceAll(/\d*?\./g, '+');
   const replaced = convertHeadNumber;
   return text.replace(headText[0], replaced);
 };
