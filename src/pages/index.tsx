efa5f7766ae8a6cb9fac62ec1babbaaee4816de3
sajiki <m.sajiki@secure-cycle.co.jp>
2023-02-05 19:15:31 +0900

    add ver

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,32 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { ChangeEvent, FC, useCallback, useEffect, useMemo, useState } from 'react';
 import TextField from '@mui/material/TextField';
-import { Grid, IconButton, InputAdornment, OutlinedInput, Typography } from '@mui/material';
+import { Grid, IconButton, InputAdornment, Link, OutlinedInput, Typography } from '@mui/material';
 import { convertBacklogNotation } from 'logics';
 import { FileCopyOutlined } from '@mui/icons-material';
+import GitHubIcon from 'assets/github-mark.svg';
 
 const Index: FC = () => {
-  const [input, setInput] = useState<string>(`# 見出し1
+  const initialInput = useMemo(
+    () => `# 見出し1
 * あいうえお
 	
 ## 見出し2
 - [ ] hoge
-		- [ ] fuga
-			- [ ] fuga2
-				- [ ] fuga3
-	
+    - [ ] fuga
+      - [ ] fuga2
+        - [ ] fuga3
+		
 ### 見出し3
 1. あいうえお
 2. かきくけこ
-3. さしすせそ`);
+		- hoge
+		- fuga
+		- piyo
+3. さしすせそ`,
+    []
+  );
+  const [input, setInput] = useState<string>(initialInput);
   const convert = useMemo(() => convertBacklogNotation(input), [input]);
 
   const [converted, setConverted] = useState<string>(convert);
@@ -35,10 +43,9 @@ const Index: FC = () => {
       <Grid item xs={6}>
         <TextField
           id="outlined-multiline-static"
-          label="Markdown"
           multiline
           rows={20}
-          defaultValue={input}
+          placeholder={input}
           sx={{ width: '100%' }}
           onChange={handleChange}
         />
@@ -67,8 +74,17 @@ const Index: FC = () => {
           label="Password"
         />
       </Grid>
-      <Grid item xs={6}>
-        <Typography>README</Typography>
+      <Grid item xs={12}>
+        <Typography variant="h4" component="span">
+          Markdown 2 Backlog
+        </Typography>
+        <Typography variant="h6" component="span" sx={{ mx: 1 }}>
+          {process.env.npm_package_version || 'v0.1.0'}
+        </Typography>
+        <Link href="https://github.com/Masumi-SAJIKI/markdown2backlog" target="_blank">
+          <img src={GitHubIcon} style={{ width: 28 }} />
+        </Link>
+        <Typography>プレースホルダーに記載してあるMarkdown記法をBacklog記法に変換できます。</Typography>
       </Grid>
     </Grid>
   );
